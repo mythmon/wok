1cdb27ecd5e7eb19313db2104b3487210aa756ce
Mike Cooper <mythmon@gmail.com>
2011-02-04 00:24:45 -0800

    Added templates (with jinja2) and page meta-data (with yaml).

--- wok.py
+++ wok.py
@@ -4,36 +4,60 @@ import sys
 import os
 import markdown
 import re
+import jinja2
+import yaml
 
-all_pages = []
+tmpl_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates/'))
 
 class Page(object):
+
     def __init__(self, path):
+        self.header = None
         self.original = None
         self.parsed = None
+        self.meta = {}
 
-        self.path, self.filename = os.path.split(path)
-        self.name = re.match(r'^(.*)\.mkd$', self.filename).group(1)
+        self.path = path
+        dir, filename = os.path.split(path)
+        self.name = re.match(r'^(.*)\.mkd$', filename).group(1)
 
         with open(path) as f:
             self.original = f.read()
-            self.parsed = markdown.markdown(self.original)
+            splits = self.original.split('---')
+            header = splits[0]
+            self.original = splits[1]
+            self.content = markdown.markdown(self.original)
+            self.meta = yaml.load(header)
 
-    def writeToFile(self, path = None):
-        if path == None:
-            path = os.path.join(self.path, self.name + '.html')
-        with open(path, 'w') as f:
-            f.write(self.parsed)
+        self.render()
+
+    def render(self):
+        type = self.meta.get('type', 'default')
+        template = tmpl_env.get_template(type + '.html')
+        templ_vars = {
+            'page': {
+                'content': self.content,
+                'title': self.meta['title'],
+            }
+        }
+        self.html = template.render(templ_vars)
+
+    def write(self, dir, filename=None):
+        if filename == None:
+            filename = self.name + '.html'
+        with open(os.path.join(dir, filename), 'w') as f:
+            f.write(self.html)
 
 def main():
-    all_pages = []
+
+    if not os.path.isdir('output'):
+        os.mkdir('output')
 
     for root, dirs, files in os.walk('.'):
         # Grab all the markdown files
         for f in [f for f in files if f[-4:] == '.mkd']:
             p = Page(os.path.join(root,f))
-            all_pages.append(p)
-            p.writeToFile()
+            p.write('output')
 
 if __name__ == '__main__':
     main()
