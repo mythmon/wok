b483ed4cb4db1e116a2348d03f28ef141a566005
Corbin Simpson <simpsoco@osuosl.org>
2011-05-07 15:01:32 -0700

    util: Fix formats for Python 2.x.
    Hmm... Is this code supposed to be 2/3 compliant? :c

--- wok/util.py
+++ wok/util.py
@@ -8,17 +8,17 @@ class out(object):
     @classmethod
     def error(cls, kind, message):
         if cls.level >= 0:
-            print("Error from {}: {}".format(kind, message))
+            print("Error from {0}: {1}".format(kind, message))
 
     @classmethod
     def warn(cls, kind, message):
         if cls.level >= 1:
-            print("Warning from {}: {}".format(kind, message))
+            print("Warning from {0}: {1}".format(kind, message))
 
     @classmethod
     def debug(cls, kind, message):
         if cls.level >= 2:
-            print("Debug from {}: {}".format(kind, message))
+            print("Debug from {0}: {1}".format(kind, message))
 
 # From http://flask.pocoo.org/snippets/5/
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
