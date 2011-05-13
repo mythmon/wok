6422d7cb02db79cd2dc977ba0338daaabeceeb26
Mike Cooper <mythmon@gmail.com>
2011-05-13 08:24:08 -0700

    Cleanup.

--- wok/util.py
+++ wok/util.py
@@ -27,7 +27,10 @@ out = Out()
 # From http://flask.pocoo.org/snippets/5/
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
 def slugify(text, delim=u'-'):
-    """Generates a ASCII-only slug."""
+    """
+    Generates a slug that will only use ASCII, be all lowercase, have no
+    spaces, and otherwise be nice for filenames, identifiers, and urls.
+    """
     result = []
     for word in _punct_re.split(text.lower()):
         word = normalize('NFKD', unicode(word)).encode('ascii', 'ignore')
