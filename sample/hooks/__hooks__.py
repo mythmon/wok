16406fc9798c5d1563e33c7fb771af59880f7615
Mike Cooper <mythmon@gmail.com>
2011-10-20 21:24:09 -0700

    Add some debugging hooks to the sample site.

---
+++ sample/hooks/__hooks__.py
@@ -0,0 +1,18 @@
+import logging
+
+hooks = {
+    'page.template.pre': []
+}
+
+try:
+    import wok_sample_hooks
+    hooks['page.render.pre'].append(wok_sample_hooks.import_hook)
+except:
+    logging.warning("Failed to import hook.")
+
+def basic_hook(page, templ_vars):
+    logging.info('basic_hook got page 0[slug]}.'.format(page))
+
+hooks['page.template.pre'].append(basic_hook)
+
+logging.info('Got hooks: {0}'.format(hooks))
---
+++ sample/hooks/wok_sample_hooks.py
@@ -0,0 +1,2 @@
+def import_hook(page, templ_vars):
+    logging.info('import_hook got page {0[slug]}.', page.keys())
