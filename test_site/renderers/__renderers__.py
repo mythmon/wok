a6e89fbcb974e30ee66f796b7a5dc3fa71b9dac8
David Soulayrol <david.soulayrol@gmail.com>
2014-08-29 21:27:56 +0200

    Added documentation and functional tests for renderers custom setup.

---
+++ test_site/renderers/__renderers__.py
@@ -0,0 +1,21 @@
+import logging
+
+try:
+    from bs4 import BeautifulSoup
+    def render(plain):
+        soup = BeautifulSoup(plain)
+        return soup.body
+
+except ImportError:
+    import cgi
+    logging.warning('HTML rendering relies on the BeautifulSoup library.')
+    def render(plain):
+        return '<h1>Rendering error</h1>' \
+            + '<p><code>BeautifulSoup</code> could not be loaded.</p>' \
+            + '<p>Original plain document follows:</p>' \
+            + '<p>' + cgi.escape(plain) + '</p>'
+
+
+renderers = {
+    'html': type('',(object,), { 'render': staticmethod(render) })
+}
