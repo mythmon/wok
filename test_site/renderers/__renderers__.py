ee85fe95bd56df0c505d3e78362b7ccfe7e38d36
Christian Ziemski <czml1@gmx.de>
2016-01-14 22:53:31 +0100

    Change test_site too (trial)

--- test_site/renderers/__renderers__.py
+++ test_site/renderers/__renderers__.py
@@ -2,14 +2,14 @@ import logging
 
 try:
     from bs4 import BeautifulSoup
-    def render(plain):
+    def render(plain, page_meta):
         soup = BeautifulSoup(plain)
         return soup.body
 
 except ImportError:
     import cgi
     logging.warning('HTML rendering relies on the BeautifulSoup library.')
-    def render(plain):
+    def render(plain, page_meta):
         return '<h1>Rendering error</h1>' \
             + '<p><code>BeautifulSoup</code> could not be loaded.</p>' \
             + '<p>Original plain document follows:</p>' \
